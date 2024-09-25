76ca0da783fc7f154bcdd65e91826ba64b37aa2c
Jean Raphael Barbosa Santos <jrbasan@hotmail.com>
2024-09-25 16:09:56 -0700

    intro to js done

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop(name);
+}
+
+
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(name);
+}
+
+
+function appendCat(name) {
+    const newArray = [...cats, name];
+    return newArray;
+}
+
+function prependCat(name) {
+    const newArray2 = [name, ...cats];
+    return newArray2;
+}
+
+function removeLastCat() {
+    const removeArr = cats.slice(0, 2);
+    return removeArr;
+}
+
+function removeFirstCat() {
+    const removeFirst = cats.slice(1);
+    return removeFirst;
+}
